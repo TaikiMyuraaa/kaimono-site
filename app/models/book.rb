68182c5a5c65e725499fb82c72b6407137014847
EC2 Default User <ningendatoomowaremasu@gmail.com>
2019-04-04 15:47:27 +0000

    test

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   require './lib/Search.rb'
-  has_one :BookType
+  has_many :BookType
   
   validates :product_id, presence: true, length: { minimum: 3, message: '入力必須項目です'}
   validates :aouthor_id, presence: true
@@ -13,13 +13,16 @@ class Book < ApplicationRecord
   def self.search(key)
     c = Search.new(key)
     if c.keyword.match(/[ |　]/)
-      like_str = c.like_str
-      str = key.gsub(/[ |　]+/,' ')
-      arr = str.split(" ")
-      @books = Book.where(like_str,* arr.map{|w| "%#{w}%"})
+      # # p search.method_defined?(:split_key)
+      # split_arr = c.like_str
+      # # split_arr = split_arr.map{|str| "%#{str}%"}
+      # str = split_arr.join(" ")
+      # binding.pry
+      # sql = Book.ransack(title_matches_any: %Q(str)).result
     else
-      @books = Book.where('title LIKE ? ', "%#{key}%")
+      @books = Book.ransack(title_matches: ["%#{key}%"]).result
     end
+    
   end
 
 end
--- lib/Search.rb
+++ lib/Search.rb
@@ -2,7 +2,7 @@ class Search
   attr_accessor :keyword
   
   def initialize(keyword)
-    @keyword = keyword.strip
+    @keyword = keyword
   end
   
   def keyword
@@ -11,14 +11,9 @@ class Search
   
   def like_str
     #文字列を半角スペース区切りにする
-    key = @keyword
-    key.gsub!(/[ |　]+/,' ')
-    
+    key = @keyword.gsub(/[ |　]+/,' ')
     key_arr = key.split(' ')
-    # 複数LIKE文作成
-    buf = (['title LIKE ?'] * key_arr.size).join(' AND ')
-    return buf
-    # return buf.chop!
+    return key_arr
     
   end
   
